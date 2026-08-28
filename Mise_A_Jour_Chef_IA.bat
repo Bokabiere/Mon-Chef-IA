0d9e900ae33658a5cda0ddc252b921bdc7ae48c5
Bokabiere <87911931+Bokabiere@users.noreply.github.com>
2026-08-29 00:41:44 +0200

    Sauvegarde avant refonte de l'interface mobile

---
+++ Mise_A_Jour_Chef_IA.bat
@@ -0,0 +1,26 @@
+@echo off
+color 0A
+echo =========================================
+echo    MISE A JOUR DE MON CHEF IA SUR GITHUB
+echo =========================================
+echo.
+cd /d "c:\IA\Projets\Mon chef_IA"
+
+echo 1. Enregistrement des fichiers (index.html, mascotte.webm et dossier Img)...
+git add index.html
+git add mascotte.webm
+git add Img
+
+echo 2. Validation...
+git commit -m "Mise a jour automatique de l'interface"
+
+echo 3. Envoi vers le serveur GitHub...
+git push -u origin main --force
+
+echo.
+echo =========================================
+echo SI AUCUNE ERREUR N'EST AFFICHEE :
+echo Mise a jour terminee avec succes ! 
+echo Vos modifications apparaitront sur le PC et le telephone d'ici 2 a 3 minutes.
+echo =========================================
+pause
