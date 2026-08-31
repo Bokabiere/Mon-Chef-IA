61ea2776323013fe1cc68a8ab1e6f356df5b14e2
Bokabiere <87911931+Bokabiere@users.noreply.github.com>
2026-08-31 18:34:31 +0200

    Mise a jour automatique de l'interface

--- sw.js
+++ sw.js
@@ -1,16 +1,26 @@
-const CACHE_NAME = 'chef-ia-v2';
+const CACHE_NAME = 'chef-ia-v3';
 const ASSETS = [
   './',
   './index.html',
   './style.css',
   './app.js',
   './manifest.json',
-  './Icone.png'
+  './Icone.png',
+  './Icone-192.png'
 ];
 
 self.addEventListener('install', (e) => {
+  self.skipWaiting();
   e.waitUntil(
     caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((names) =>
+      Promise.all(names.filter((n) => n !== CACHE_NAME).map((n) => caches.delete(n)))
+    ).then(() => self.clients.claim())
   );
 });
 
